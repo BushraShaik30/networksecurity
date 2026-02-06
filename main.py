de9c49fcf52171aeac4f4354d038418f6b79f68d
Bushra <bushrashaik05@gmail.com>
2026-02-06 18:28:21 +0530

    trainer evaluation and mlflow committed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation,DataValidationConfig
 from networksecurity.components.data_transformation import DataTransformation,DataTransformationConfig
+from networksecurity.components.model_trainer import ModelTrainer,ModelTrainerConfig
 import sys
 
 if __name__=="__main__":
@@ -30,6 +31,14 @@ if __name__=="__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("Data Transformation completed")
+
+        logging.info("Model Training started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact= model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+
 
 
 
